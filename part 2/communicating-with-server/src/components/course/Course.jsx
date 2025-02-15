a3bafe95065c65ed2ac3e72df7694584f30ce3e3
semiarte <elsemi6@gmail.com>
2025-02-15 17:54:50 +0100

    exercises 2.4 and 2.5

--- part 2/communicating-with-server/src/components/course/Course.jsx
+++ part 2/communicating-with-server/src/components/course/Course.jsx
@@ -2,25 +2,18 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 
-const Course = (props) => {
-    const { course } = props;
+const Course = ({ courses }) => {
     return(
         <>
-            <Header name={course.name} />
-            <Content 
-                part={course.parts[0].name} 
-                exercises={course.parts[0].exercises} 
-            />
-            <Content 
-                part={course.parts[1].name} exercises={props.course.parts[1].exercises} 
-            />
-            <Content 
-                part={course.parts[2].name} exercises={props.course.parts[2].exercises} 
-            />
-            <Content 
-                part={course.parts[3].name} exercises={props.course.parts[3].exercises} 
-            />
-            <Total parts={course.parts} />
+            {courses.map(({name, id, parts}) => {
+                return(
+                    <div className="course" key={id}>
+                        <Header name={name} />
+                        { parts.map(part => <Content key={part.id} part={part.name} exercises={part.exercises}/>)}
+                        <Total parts={parts} />
+                    </div>
+                );
+            })}
         </>
     );
 }
---
+++ part 2/communicating-with-server/src/components/course/index.js
@@ -0,0 +1,4 @@
+export { default as Course } from   '../course/Course';
+// export { default as Header } from '../course/Header';
+// export { default as Content } from '../course/Content';
+// export { default as Total } from '../course/Total';
