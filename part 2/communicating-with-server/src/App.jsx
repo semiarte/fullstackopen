3a785270590475e9ab3fe3d483b02594208321fe
semiarte <elsemi6@gmail.com>
2025-02-22 18:11:34 +0100

    exercises 2.6 and 2.7

--- part 2/communicating-with-server/src/App.jsx
+++ part 2/communicating-with-server/src/App.jsx
@@ -1,52 +1,59 @@
-import { Course } from "./components/course"
-/* eslint-disable react/prop-types */
+import { useState } from "react"
+
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", id: "1" }
+  ])
+  // Controls input element
+  const [newName, setNewName] = useState('')
+  const handleInputNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+  // Adds new contact
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    const personObject = { 
+      name: newName,
+      id: String(persons.length + 1),
     }
-  ]
+    if (persons.some(person => person.name === newName)) {
+      // Determines whether the specified callback function returns true for any element of an array
+      alert(`${newName} is alredy added to phonebook`)
+      return
+    } else {
+      // Returns a new array without modifying initial and previous arrays
+      setPersons(persons.concat(personObject))
+      setNewName("")
+    }
+  }
+  
   return (
-    <Course courses={courses} />
+    <>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: 
+          <input 
+            type="text"
+            value={newName}
+            onChange={handleInputNameChange}
+          />
+        </div>
+
+        <div>
+          <button 
+            type="submit"
+            onClick={addNewPerson}
+          >
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(
+        person => <p key={person.id}>{person.name}</p>
+      )}
+    </>
   )
 }
 
