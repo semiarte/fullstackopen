d5e85e70cf8f23ea4075d1192f8ef373137cccc0
semiarte <elsemi6@gmail.com>
2025-04-05 13:07:47 +0200

    exercise 2.11

--- part 2/communicating-with-server/src/App.jsx
+++ part 2/communicating-with-server/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from 'axios'
 import { Filter, PersonForm, Persons } from './components/phonebook'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", id: "1", number: "040-44-532523" },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  // Modify the application such that the initial state of the data is fetched from the server using the axios-library. 
+  // Complete the fetching with an Effect hook.
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+    })
+  }, [])
+
   // Controls input element
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
